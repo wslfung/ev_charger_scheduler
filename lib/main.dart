e740ad9d108902c49a7ba939065564ffb9426313
Wilson Fung <wilson.sl.fung@gmail.com>
2019-04-28 17:34:14 -0700

    added a few widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,85 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HECSApp());
 
-class MyApp extends StatelessWidget {
+class HECSApp extends StatelessWidget {
+
+  Widget locationBar() {
+    return Padding(
+      child: Row(
+        children: [
+          Text("Location: ", style: TextStyle(fontSize: 16)),
+          Padding(
+              child: DropdownButton<String>(
+                  items: [
+                    DropdownMenuItem(child: Text("Broadway Green")),
+                  ],
+                  onChanged: locationDropdownOnChange),
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 0)
+          )
+        ],
+      ),
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+    );
+  }
+
+  void locationDropdownOnChange(String newValue) {
+
+  }
+
+  Widget infoBar() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Card(
+            margin: EdgeInsets.all(10),
+            child: Container(
+              padding: EdgeInsets.fromLTRB(10, 50, 5, 50),
+              child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.place),
+                        tooltip: 'Location Information',
+                        onPressed: () {
+                        },
+                        iconSize: 50,
+                        color: Color.fromRGBO(255, 50, 50, 1),
+                      ),
+                    ],
+                  )
+              ),
+            ),
+          ),
+          key: Key("locationInfo"),
+        ),
+        Expanded(
+          child: Card(
+            margin: EdgeInsets.all(10),
+            child: Container(
+              padding: EdgeInsets.fromLTRB(5, 50, 10, 50),
+              child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.schedule),
+                        tooltip: 'Queue Information',
+                        onPressed: () {
+                        },
+                        iconSize: 50,
+                        color: Color.fromRGBO(255, 50, 50, 1),
+                      ),
+                    ],
+                  )
+              ),
+            ),
+          ),
+          key: Key("queueInfo"),
+        ),
+      ],
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,92 +97,20 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("EV Charger Scheduler"),
+        ),
+        body: ListView(
+          children: [
+            locationBar(),
+            infoBar(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
+//      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+
